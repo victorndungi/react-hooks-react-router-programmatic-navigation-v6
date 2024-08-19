efffdf25a0c81a9a332eaac1ae68eca814138430
victorndungi <victor.ndungi@student.moringaschool.com>
2024-08-19 20:55:42 +0300

    Add navigate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet,useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { useState,useEffect } from "react";
 
 function App() {
+  const[isLoggedIn, setIsLoggedIn]= useState(false)
+const navigate=useNavigate()
 
+const login = () => {
+  setIsLoggedIn(true);
+}
+const logout = () =>{
+  setIsLoggedIn(false);
+}
 
+useEffect (()=>{
+  if(isLoggedIn){
+    navigate("/")
+  }else{
+    navigate("/login")
+  };
+}, [isLoggedIn])
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+     {isLoggedIn ? <NavBar logout={logout }/> : <Navigate to ="/login"/>}
+      <Outlet login={login} />
     </div>
   );
 }
